c935d57bbea9e74195843f8ca5c47f66e85a1a39
Marc Gallant <marc.gallant@quanergy.com>
2016-10-24 14:17:41 -0400

    Minor fix to Card constructor.

--- include/crib_hand_helper/card.h
+++ include/crib_hand_helper/card.h
@@ -1,19 +1,20 @@
 #ifndef CARD_H
 #define CARD_H
 
-#include <vector>
 #include <string>
+#include <vector>
 
 struct Card
 {
   Card() = delete;
   Card(unsigned rank, char suit);
   Card(char name, char suit);
-  Card(std::string& name_and_suit) : Card(name_and_suit[0], name_and_suit[1]) {};
-  unsigned rank; // 1 (ace) to 13 (king)
-  unsigned value; // 1-9 (A-9) and 10 (T-K)
-  char name; // 'A', '2', ..., 'T', 'J', 'Q', 'K'
-  char suit; // 'c', 'd', 'h', or 's'
+  Card(const std::string& name_and_suit)
+    : Card(name_and_suit[0], name_and_suit[1]){};
+  unsigned rank;   // 1 (ace) to 13 (king)
+  unsigned value;  // 1-9 (A-9) and 10 (T-K)
+  char name;       // 'A', '2', ..., 'T', 'J', 'Q', 'K'
+  char suit;       // 'c', 'd', 'h', or 's'
 };
 
 using Hand = std::vector<Card>;
